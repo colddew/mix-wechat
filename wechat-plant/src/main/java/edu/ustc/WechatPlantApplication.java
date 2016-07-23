31193f3fac4ec59eb47c2189c5969630d5ce84e8
colddew <88028842@qq.com>
2016-07-24 04:12:49 +0800

    add static resources

--- wechat-plant/src/main/java/edu/ustc/WechatPlantApplication.java
+++ wechat-plant/src/main/java/edu/ustc/WechatPlantApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WechatPlantApplication {
-	
 	public static void main(String[] args) throws Exception {
 		new SpringApplication(WechatPlantApplication.class).run(args);
     }
