45d7abb29d5ee6795466e73163afa348a76ed25c
colddew <88028842@qq.com>
2016-07-24 03:51:13 +0800

    init wechat

---
+++ wechat-plant/src/main/java/edu/ustc/WechatPlantApplication.java
@@ -0,0 +1,12 @@
+package edu.ustc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatPlantApplication {
+	
+	public static void main(String[] args) throws Exception {
+		new SpringApplication(WechatPlantApplication.class).run(args);
+    }
+}
